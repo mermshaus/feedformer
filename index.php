9475ea27ec2addf5fee928f57f2cca7b8f4ece38
Marc Ermshaus <marc@ermshaus.org>
2016-12-15 03:21:31 +0100

    Remove explicit error reporting settings (bad style).

--- index.php
+++ index.php
@@ -9,9 +9,6 @@ use FeedFormer\Application;
 
 use Symfony\Component\HttpFoundation\Request;
 
-error_reporting(-1);
-ini_set('display_errors', 1);
-
 require __DIR__ . '/vendor/autoload.php';
 
 $feedFormer = new Application();
